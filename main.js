d1575d26c639a450f834377670cda43ac76c862e
Nick Kortendick <nick@nickkortendick.com>
2017-04-22 18:04:43 -0500

    View changes

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
 InboxSDK.load('1.0', 'sdk_stegosaur_1b4904456f').then(function(sdk){
   sdk.Compose.registerComposeViewHandler(function(composeView){
-    // // a compose view has come into existence, do something with it!
-    // composeView.addButton({
-    //   title: "Stegosaur",
-    //   iconUrl: 'http://cliparts.co/cliparts/pTq/KMd/pTqKMdG6c.png',
-    //   onClick: function(event) {
-    //     // Show Div for Ima
-    //     // getToRecipients()
-    //     event.composeView.insertHTMLIntoBodyAtCursor('<img src=\"http://news.nationalgeographic.com/content/dam/news/2016/04/21/01-baby-dinosaurs.jpg\" />');
-    //   }
-    // });
-
     var statusBarView = composeView.addStatusBar({
       height: 40
     });
+
     $.get(chrome.extension.getURL('imageselect.html'), function(data) {
       statusBarView.el.innerHTML = data;
-      $(statusBarView.el).find("#steg-insert").click(function() {
-        var imageAddress = $(statusBarView.el).find("#steg-address").val();
-
-        // load image
-
-        // convert image to library
-
-        // pgp
-
-        // copy image to clipboard
-
-        // trigger paste event in gmail
-        // $(composeView).trigger("paste");
-        composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ imageAddress + '\" />');
-      })
+      bindAddressInputToImage(statusBarView.el);
     });
   });
 });
+
+function bindAddressInputToImage(statusBar) {
+  $(statusBar).find("#steg-address").change(function() {
+    var imageAddress = $(statusBar).find("#steg-address").val();
+    $(statusBar).find("#steg-image").attr('src', imageAddress);
+  });
+}
+
+function uploadToImgur(imageBase64) {
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.imgur.com/3/upload',
+    crossDomain: true,
+    headers: {
+      Authorization: 'Client-ID 977be0be51e54df',
+      Accept: 'application/json'
+    },
+    data: {
+      image: imageBase64,
+      type: 'base64'
+    },
+    success: function(response) {
+      console.log(response.data);
+      composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ response.data.link + '\" />');
+    },
+    error: function (response) {
+      console.log(response);
+      alert('POST failed.');
+    }
+  });
+}
