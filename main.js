3cb81fb4145ac7a162fd852cb8a112b8d908473d
Nick Kortendick <nick@nickkortendick.com>
2017-04-22 19:13:47 -0500

    Add some boilerplate

--- main.js
+++ main.js
@@ -6,16 +6,45 @@ InboxSDK.load('1.0', 'sdk_stegosaur_1b4904456f').then(function(sdk){
 
     $.get(chrome.extension.getURL('imageselect.html'), function(data) {
       statusBarView.el.innerHTML = data;
-      bindAddressInputToImage(statusBarView.el);
+      bindAddressInputToPreviewImage(statusBarView.el);
+      insertImageToThreadOnClick(statusBarView.el, composeView);
     });
   });
 });
 
-function bindAddressInputToImage(statusBar) {
+function insertImageToThreadOnClick(statusBar, composeView) {
+  $(statusBar).find("#steg-insert").click(function() {
+    var canvas = $(statusBar).find("#steg-canvas").get(0);
+
+    // Do Steganography here
+    // http://stackoverflow.com/questions/667045/getpixel-from-html-canvas
+
+
+    var imageText = canvas.toDataURL();
+    composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ imageText + '\" />');
+  });
+}
+
+function bindAddressInputToPreviewImage(statusBar) {
   $(statusBar).find("#steg-address").change(function() {
     var imageAddress = $(statusBar).find("#steg-address").val();
     $(statusBar).find("#steg-image").attr('src', imageAddress);
+    insertImageToCanvas(statusBar, imageAddress);
   });
+}
+
+/* The only way we can edit our image is if it's in a canvas. But it must come from a CORS-enabled domain and not animated.
+The canvas is drawn but hidden, and it is a square and may need to be redrawn or multiple images */ 
+function insertImageToCanvas(statusBar, imageAddress) {
+  var image = new Image();
+  image.crossOrigin = 'anonymous';
+  image.onload = function () {
+    var canvas = $(statusBar).find("#steg-canvas").get(0);
+    var context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.width * 1.0 * image.width / image.height);
+    var imageText = canvas.toDataURL();
+  };
+  image.src = imageAddress;
 }
 
 function uploadToImgur(imageBase64) {
@@ -33,7 +62,7 @@ function uploadToImgur(imageBase64) {
     },
     success: function(response) {
       console.log(response.data);
-      composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ response.data.link + '\" />');
+      // composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ response.data.link + '\" />');
     },
     error: function (response) {
       console.log(response);
