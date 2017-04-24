2fe12021e6e5251d31b3cbe38bb5557234887384
Sujay Khandekar <khandek2@illinois.edu>
2017-04-24 15:49:17 -0500

    lol

--- main.js
+++ main.js
@@ -15,10 +15,58 @@ InboxSDK.load('1.0', 'sdk_stegosaur_1b4904456f').then(function(sdk){
 function insertImageToThreadOnClick(statusBar, composeView) {
   $(statusBar).find("#steg-insert").click(function() {
     var canvas = $(statusBar).find("#steg-canvas").get(0);
+    console.log($('#steg-address').val(), $('#steg-message').val())
+
 
     // Do Steganography here
     // http://stackoverflow.com/questions/667045/getpixel-from-html-canvas
 
+    var message =  $('#steg-message').val();
+    var password = "mypassword"; // Password will go here
+    if (password.length > 0) {
+      message = sjcl.encrypt(password, message);
+    } else {
+      message = JSON.stringify({'text': message});
+    }
+
+    var getBit = function(number, location) {
+      return ((number >> location) & 1);
+    };
+
+    var getBitsFromNumber = function(number) {
+      var bits = [];
+      for (var i = 0; i < 16; i++) {
+        bits.push(getBit(number, i));
+      }
+      return bits;
+    };
+
+    var setBit = function(number, location, bit) {
+      return (number & ~(1 << location)) | (bit << location);
+    };
+
+    var messageBits = [];
+    for (var i = 0; i < message.length; i++) {
+      var code = message.charCodeAt(i);
+      var bits = getBitsFromNumber(code);
+      messageBits = messageBits.concat(bits);
+    }
+
+    var ctx = canvas.getContext("2d");
+    var imgData = ctx.getImageData(0,0, canvas.width, canvas.height);
+    var colors = imgData.data;
+    console.log(colors)
+
+    var hash = sjcl.hash.sha256.hash(password);
+    var pos = 0;
+    while (pos < messageBits.length) {
+      var rand = hash[pos % hash.length] * (pos + 1);
+      var loc = Math.abs(rand) % colors.length;
+      colors[loc] = setBit(colors[loc], 0, messageBits[pos]);
+      pos++;
+    }
+    imgData.data = colors
+    ctx.putImageData(imgData, 0, 0)
 
     var imageText = canvas.toDataURL();
     imageText = imageText.split("base64")[1];
@@ -36,7 +84,7 @@ function bindAddressInputToPreviewImage(statusBar) {
 }
 
 /* The only way we can edit our image is if it's in a canvas. But it must come from a CORS-enabled domain and not animated.
-The canvas is drawn but hidden, and it is a square and may need to be redrawn or multiple images */ 
+The canvas is drawn but hidden, and it is a square and may need to be redrawn or multiple images */
 function insertImageToCanvas(statusBar, imageAddress) {
   var image = new Image();
   image.crossOrigin = 'anonymous';
@@ -63,12 +111,11 @@ function uploadToImgurAndInsertToThread(composeView, imageBase64) {
       type: 'base64'
     },
     success: function(response) {
-      console.log(response.data);
+      console.log(response);
       composeView.insertHTMLIntoBodyAtCursor('<img src=\"'+ response.data.link + '\" />');
     },
     error: function (response) {
-      console.log(response);
-      alert('POST failed.');
+      alert('Image failed to insert.');
     }
   });
 }
